74cdbe8a3db6faab742b1f9f803b82b9087571d0
LZY_Shrio <170280176@qq.com>
2022-05-10 14:25:08 +0800

    用户详情

--- src/components/UserDrawer.vue
+++ src/components/UserDrawer.vue
@@ -1,11 +1,19 @@
 <template>
-  <el-drawer
-    v-model="visable"
-    title="用户详情"
-    :with-header="false"
-    @close="handleClose"
-  >
-    <span>{{ user?.name }}</span>
+  <el-drawer v-model="visable" title="详情" :size="700" @close="handleClose">
+    <span>
+      <el-avatar shape="square" :size="100" :fit="fit" :src="user?.img" />
+    </span>
+    <el-descriptions border direction="vertical" :column="4" title="用户信息">
+      <el-descriptions-item label="用户名">{{
+        user?.name
+      }}</el-descriptions-item>
+      <el-descriptions-item label="邮箱地址">{{
+        user?.email
+      }}</el-descriptions-item>
+      <el-descriptions-item label="Remarks">
+        <el-tag size="small">{{ user?.tag }}</el-tag>
+      </el-descriptions-item>
+    </el-descriptions>
   </el-drawer>
 </template>
 
