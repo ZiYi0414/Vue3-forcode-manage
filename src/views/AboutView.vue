1a7bb755ccfb66dfe773f147ef6a569fb08c92f2
LZY_Shrio <170280176@qq.com>
2022-05-10 14:28:47 +0800

    feat：关于页面

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>这是一个关于页面。我是一个辣鸡项目</h1>
+    <a href="https://github.com/ZYKKOne-11/Vue3-forcode-manage" target="_blank"
+      >前端源码地址</a
+    >
   </div>
 </template>
